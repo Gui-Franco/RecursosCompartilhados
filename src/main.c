98f38363e06ad1fea75952d694d9f5b5a1d86a39
Guilherme Franco <guifrancorod@yahoo.com.br>
2026-06-16 10:14:43 -0300

    Parte 1 (Sem sincronização)

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
+
+#define STACK_SIZE 1024
+#define PRIORITY 5 //IGUAL INICIALMENTE
+
+volatile int saldo_vitrine = 0;
+
+
+void Padeiro(void *arg1, void *arg2, void *arg3)
+{
+    while(1) {
+        //Padeiro produz um pão a cada 1 segundo
+        k_sleep(K_SECONDS(1));
+
+        saldo_vitrine++; // Adiciona um pão à vitrine
+        printk("Padeiro produziu um pão. Saldo atual na vitrine: % d\n", saldo_vitrine);
+    }
+}
+
+void Cliente(void *arg1, void *arg2, void *arg3)
+{
+    while(1) {
+        //Cliente compra um pão a cada 1.5 segundo
+        k_sleep(K_MSEC(1500)); 
+
+        saldo_vitrine--; // Cliente compra um pão da vitrine
+        printk("Cliente comprou um pão. Saldo atual na vitrine: % d\n", saldo_vitrine);
+    }
+}
+
+K_THREAD_DEFINE(padeiro_tid, STACK_SIZE, Padeiro, NULL, NULL, NULL,
+                PRIORIY, 0, 0);
+K_THREAD_DEFINE(cliente_tid, STACK_SIZE, Cliente, NULL, NULL, NULL,
+                PRIORITY, 0, 0);
+
+
+void main (void){
+    
+    printk("Padaria Aberta (Sem sincronização).\n");
+
+    while(1) {
+        k_sleep(K_MSEC(1000));
+    }
+
+}
